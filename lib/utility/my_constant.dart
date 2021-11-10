a7e1603ee7a0cf30c185d067e7acad412a76601c
wintech <wintech_thekop@hotmail.com>
2021-11-11 06:25:36 +0700

    Ep6.1 การ Route ไป page อื่นด้วย map

---
+++ lib/utility/my_constant.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MyConstant {
+  //Generally
+
+  static String appName = "Phichii Mall";
+
+  //Routes
+  static String routeAuthen = "/authen";
+  static String routeCreateAccount = "/createAccount";
+  static String routeBuyerService = "/buyerService";
+  static String routeSellerService = "/sellerService";
+  static String routeRiderService = "/riderService";
+}
