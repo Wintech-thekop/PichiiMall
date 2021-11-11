ebc1c00a73052f373d5ed3782bdf52fcae891b44
wintech <wintech_thekop@hotmail.com>
2021-11-11 22:37:17 +0700

    EP6.2 การนำรูปภาพมาใช้ในแอพ

--- lib/state/authen.dart
+++ lib/state/authen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:pichiimall/utility/my_constant.dart';
+import 'package:pichiimall/widgets/show_image.dart';
 
 class Authen extends StatefulWidget {
-  const Authen({ Key? key }) : super(key: key);
+  const Authen({Key? key}) : super(key: key);
 
   @override
   _AuthenState createState() => _AuthenState();
@@ -10,8 +12,14 @@ class Authen extends StatefulWidget {
 class _AuthenState extends State<Authen> {
   @override
   Widget build(BuildContext context) {
+    double size = MediaQuery.of(context).size.width; // เป็นการหาความกว้างตามขนาดหน้าจอ
     return Scaffold(
-      body: SafeArea(child: Text('This is authen')),
+      body: SafeArea(
+        child: Container(
+          width: size*0.6, // กำหนดความกว้างของรูปภาพเป็น 60% ของหน้าจอ
+          child: ShowImage(path: MyConstant.image1),
+        ),
+      ),
     );
   }
 }
--- lib/utility/my_constant.dart
+++ lib/utility/my_constant.dart
@@ -11,4 +11,10 @@ class MyConstant {
   static String routeBuyerService = "/buyerService";
   static String routeSellerService = "/sellerService";
   static String routeRiderService = "/riderService";
+
+  // Images
+  static String image1 = 'images/image1.png';
+  static String image2 = 'images/image2.png';
+  static String image3 = 'images/image3.png';
+  static String image4 = 'images/image4.png';
 }
