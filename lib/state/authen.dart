5eaf38468f373c1b17d555b4928df493b9a617ef
wintech <wintech_thekop@hotmail.com>
2021-11-12 06:24:34 +0700

    EP7 การจัดการ TextWidget และ กำหนด Style

--- lib/state/authen.dart
+++ lib/state/authen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pichiimall/utility/my_constant.dart';
 import 'package:pichiimall/widgets/show_image.dart';
+import 'package:pichiimall/widgets/show_title.dart';
 
 class Authen extends StatefulWidget {
   const Authen({Key? key}) : super(key: key);
@@ -12,14 +13,41 @@ class Authen extends StatefulWidget {
 class _AuthenState extends State<Authen> {
   @override
   Widget build(BuildContext context) {
-    double size = MediaQuery.of(context).size.width; // เป็นการหาความกว้างตามขนาดหน้าจอ
+    double size =
+        MediaQuery.of(context).size.width; // เป็นการหาความกว้างตามขนาดหน้าจอ
     return Scaffold(
       body: SafeArea(
-        child: Container(
-          width: size*0.6, // กำหนดความกว้างของรูปภาพเป็น 60% ของหน้าจอ
-          child: ShowImage(path: MyConstant.image1),
+        child: ListView(
+          children: [
+            buildImage(size),
+            buildTitle(),
+          ],
         ),
       ),
+    );
+  }
+
+  Row buildTitle() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ShowTitle(
+          title: MyConstant.appName,
+          textStyle: MyConstant().h1Style(),
+        ),
+      ],
+    );
+  }
+
+  Row buildImage(double size) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          width: size * 0.6, // กำหนดความกว้างของรูปภาพเป็น 60% ของหน้าจอ
+          child: ShowImage(path: MyConstant.image1),
+        ),
+      ],
     );
   }
 }
