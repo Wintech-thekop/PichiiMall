9e54f8b1199360bdc30abe4a75081c6a1c6c2e0b
wintech <wintech_thekop@hotmail.com>
2021-11-20 16:56:20 +0700

    EP32 การสร้าง TextFormField แบบ หลายๆ บรรทัด

--- lib/state/add_product.dart
+++ lib/state/add_product.dart
@@ -1,7 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:pichiimall/utility/my_constant.dart';
 
 class AddProduct extends StatefulWidget {
-  const AddProduct({ Key? key }) : super(key: key);
+  const AddProduct({Key? key}) : super(key: key);
 
   @override
   _AddProductState createState() => _AddProductState();
@@ -11,7 +15,116 @@ class _AddProductState extends State<AddProduct> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Add Product'),),
+      appBar: AppBar(
+        title: Text('Add Product'),
+      ),
+      body: LayoutBuilder(
+        builder: (context, constraints) => GestureDetector(
+          onTap: () => FocusScope.of(context).requestFocus(FocusNode()),
+          behavior: HitTestBehavior.opaque,
+          child: Center(
+            child: Column(
+              children: [
+                buildProductName(constraints),
+                buildProductPrice(constraints),
+                buildProductDetail(constraints),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildProductDetail(BoxConstraints constraints) {
+    return Container(
+      width: constraints.maxWidth * 0.75,
+      margin: EdgeInsets.only(top: 15),
+      child: TextFormField(
+        maxLines: 4,
+        decoration: InputDecoration(
+          hintText: 'Product detail :',
+          hintStyle: MyConstant().h3Style(),
+          prefixIcon: Padding(
+            padding: const EdgeInsets.fromLTRB(0, 0, 0, 60),
+            child: Icon(
+              Icons.details_sharp,
+              color: MyConstant.dark,
+            ),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: MyConstant.dark),
+            borderRadius: BorderRadius.circular(25),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: MyConstant.light),
+            borderRadius: BorderRadius.circular(25),
+          ),
+          errorBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.red),
+            borderRadius: BorderRadius.circular(25),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildProductPrice(BoxConstraints constraints) {
+    return Container(
+      width: constraints.maxWidth * 0.75,
+      margin: EdgeInsets.only(top: 15),
+      child: TextFormField(
+        keyboardType: TextInputType.number,
+        decoration: InputDecoration(
+          labelText: 'Product Price :',
+          labelStyle: MyConstant().h3Style(),
+          prefixIcon: Icon(
+            Icons.money_sharp,
+            color: MyConstant.dark,
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: MyConstant.dark),
+            borderRadius: BorderRadius.circular(25),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: MyConstant.light),
+            borderRadius: BorderRadius.circular(25),
+          ),
+          errorBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.red),
+            borderRadius: BorderRadius.circular(25),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildProductName(BoxConstraints constraints) {
+    return Container(
+      width: constraints.maxWidth * 0.75,
+      margin: EdgeInsets.only(top: 15),
+      child: TextFormField(
+        decoration: InputDecoration(
+          labelText: 'Product Name :',
+          labelStyle: MyConstant().h3Style(),
+          prefixIcon: Icon(
+            Icons.production_quantity_limits_sharp,
+            color: MyConstant.dark,
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: MyConstant.dark),
+            borderRadius: BorderRadius.circular(25),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: MyConstant.light),
+            borderRadius: BorderRadius.circular(25),
+          ),
+          errorBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.red),
+            borderRadius: BorderRadius.circular(25),
+          ),
+        ),
+      ),
     );
   }
 }
