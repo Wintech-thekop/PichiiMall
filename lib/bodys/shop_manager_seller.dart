200d6a97ff47e6170efd29c7ac52e10a3c5e8000
wintech <wintech_thekop@hotmail.com>
2021-12-19 12:45:07 +0700

    EP60.2  การแสดง แผนที่ พร้อม ปักหมุด จาก Server

--- lib/bodys/shop_manager_seller.dart
+++ lib/bodys/shop_manager_seller.dart
@@ -1,7 +1,8 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_collection_literals
 
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:pichiimall/models/user_model.dart';
 import 'package:pichiimall/utility/my_constant.dart';
 import 'package:pichiimall/widgets/show_progress.dart';
@@ -31,59 +32,96 @@ class _ShopManageSellerState extends State<ShopManageSeller> {
       body: LayoutBuilder(
         builder: (context, constraints) => Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              ShowTitle(
-                  title: 'Shop name : ', textStyle: MyConstant().h2Style()),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ShowTitle(
-                        title: userModel!.name,
-                        textStyle: MyConstant().h1Style()),
-                  ),
-                ],
-              ),
-              ShowTitle(title: 'Address : ', textStyle: MyConstant().h2Style()),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Container(
-                    width: constraints.maxWidth * 0.6,
-                    child: Padding(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ShowTitle(
+                    title: 'Shop name : ', textStyle: MyConstant().h2Style()),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
                       padding: const EdgeInsets.all(8.0),
                       child: ShowTitle(
-                          title: userModel!.address,
-                          textStyle: MyConstant().h2Style()),
+                          title: userModel!.name,
+                          textStyle: MyConstant().h1Style()),
                     ),
-                  ),
-                ],
-              ),
-              ShowTitle(
-                  title: 'Phone : ${userModel!.phone}',
-                  textStyle: MyConstant().h2Style()),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8),
-                child: ShowTitle(
-                    title: 'Avatar :', textStyle: MyConstant().h2Style()),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(margin: EdgeInsets.symmetric(vertical: 16),
-                    width: constraints.maxWidth * 0.6,
-                    child: CachedNetworkImage(
-                      imageUrl: '${MyConstant.domain}${userModel!.avatar}',
-                      placeholder: (context, url) => ShowProgress(),
+                  ],
+                ),
+                ShowTitle(
+                    title: 'Address : ', textStyle: MyConstant().h2Style()),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Container(
+                      width: constraints.maxWidth * 0.6,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ShowTitle(
+                            title: userModel!.address,
+                            textStyle: MyConstant().h2Style()),
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              ShowTitle(title: 'Location : ', textStyle: MyConstant().h2Style()),
-            ],
+                  ],
+                ),
+                ShowTitle(
+                    title: 'Phone : ${userModel!.phone}',
+                    textStyle: MyConstant().h2Style()),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8),
+                  child: ShowTitle(
+                      title: 'Avatar :', textStyle: MyConstant().h2Style()),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 16),
+                      width: constraints.maxWidth * 0.6,
+                      child: CachedNetworkImage(
+                        imageUrl: '${MyConstant.domain}${userModel!.avatar}',
+                        placeholder: (context, url) => ShowProgress(),
+                      ),
+                    ),
+                  ],
+                ),
+                ShowTitle(
+                    title: 'Location : ', textStyle: MyConstant().h2Style()),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 16),
+                      width: constraints.maxWidth * 0.6,
+                      height: constraints.maxWidth * 0.6,
+                      child: GoogleMap(
+                        initialCameraPosition: CameraPosition(
+                          target: LatLng(
+                            double.parse(userModel!.lat),
+                            double.parse(userModel!.lng),
+                          ),
+                          zoom: 16,
+                        ),
+                        markers: <Marker>[
+                          Marker(
+                            markerId: MarkerId('id'),
+                            position: LatLng(
+                              double.parse(userModel!.lat),
+                              double.parse(userModel!.lng),
+                            ),
+                            infoWindow: InfoWindow(
+                                title: 'You Here',
+                                snippet:
+                                    'Lat = ${userModel!.lat}, lng = ${userModel!.lng}'),
+                          ),
+                        ].toSet(),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
