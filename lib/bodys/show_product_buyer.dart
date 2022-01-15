a06cb6b9e3311409236877718a32cc0399df7937
wintech <wintech_thekop@hotmail.com>
2022-01-16 00:48:40 +0700

    EP76 การใช้ Dio Package ให้ Call API มาเป็น Json

--- lib/bodys/show_product_buyer.dart
+++ lib/bodys/show_product_buyer.dart
@@ -1,5 +1,9 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:pichiimall/models/user_model.dart';
+import 'package:pichiimall/utility/my_constant.dart';
+import 'package:pichiimall/widgets/show_progress.dart';
+import 'package:pichiimall/widgets/show_title.dart';
 
 class ShowProductBuyer extends StatefulWidget {
   final UserModel userModel;
@@ -10,20 +14,53 @@ class ShowProductBuyer extends StatefulWidget {
 }
 
 class _ShowProductBuyerState extends State<ShowProductBuyer> {
-UserModel? userModel;
+  UserModel? userModel;
+  bool load = true;
+  bool? haveProduct;
 
-
-@override
+  @override
   void initState() {
     // TODO: implement initState
     super.initState();
     userModel = widget.userModel;
+    readAPI();
   }
+
+  Future<Null> readAPI() async {
+    String urlAPI =
+        '${MyConstant.domain}/pichiimall/getProductWhereIdSeller.php?isAdd=true&idSeller=${userModel!.id}';
+    await Dio().get(urlAPI).then((value) {
+      print('value ==> $value');
+      if (value.toString() == 'null') {
+        setState(() {
+          haveProduct = false;
+          load = false;
+        });
+      } else {
+        setState(() {
+          haveProduct = true;
+          load = false;
+        });
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(userModel!.name),),
-      body: Text('This ShowProductBuyer idSeller is ${userModel!.id}'),
+      appBar: AppBar(
+        title: Text(userModel!.name),
+      ),
+      body: load
+          ? ShowProgress()
+          : haveProduct!
+              ? Text('Have Data')
+              : Center(
+                child: ShowTitle(
+                    title: 'No Prduct',
+                    textStyle: MyConstant().h1Style(),
+                  ),
+              ),
     );
   }
 }
