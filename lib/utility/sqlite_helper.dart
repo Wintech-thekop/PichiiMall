0ad0dce9cf4d5c2a853159e1c1442bda5a0edf11
wintech <wintech_thekop@hotmail.com>
2022-01-29 15:55:43 +0700

    EP82  SQFLite Package สำหรับ SQLite

---
+++ lib/utility/sqlite_helper.dart
@@ -0,0 +1,18 @@
+class SQLiteHelper {
+  final String nameDatabase = 'pichiimall.db';
+  final int version = 1;
+  final String tableDatabase = 'tableOrder';
+  final String columnId = 'id';
+  final String columnIdSeller = 'idSeller';
+  final String columnIdProduct = 'idProduct';
+  final String columnName = 'name';
+  final String columnPrice = 'price';
+  final String columnAmount = 'amount';
+  final String columnSum = 'sum';
+
+  SQLiteHelper() {
+    initialDatabase();
+  }
+
+  Future<Null> initialDatabase() async {}
+}
