931111cf85bd04ac1fe574cd5aff934d5a4efb65
wintech <wintech_thekop@hotmail.com>
2022-03-13 13:20:58 +0700

    EP102การวาดหรือตัดโค้ด Flutter จาก UI ที่ออกแบบไว้

--- lib/bodys/credit.dart
+++ lib/bodys/credit.dart
@@ -15,24 +15,105 @@ class _CreditState extends State<Credit> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          buildTitle('Name Surname'),
-          buildNameSurname(),
-          buildTitle('ID Card No.'),
-          formIDCard(),
-          Row(
-            children: [
-              Expanded(
-                child: buildTitle('Expires End : '),
-              ),
-              Expanded(
-                child: buildTitle('CVC : '),
-              ),
-            ],
+      body: GestureDetector(
+        onTap: () => FocusScope.of(context).requestFocus(FocusNode()),
+        behavior: HitTestBehavior.opaque,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            buildTitle('Name Surname'),
+            buildNameSurname(),
+            buildTitle('ID Card No.'),
+            formIDCard(),
+            buildExpiresCVC(),
+            buildTitle('Amount : '),
+            formAmount(),
+            Spacer(),
+            buildButtonAddMoney(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Container buildButtonAddMoney() {
+    return Container(
+            width: double.infinity,
+            child: ElevatedButton(
+              onPressed: () {},
+              child: Text('Add Money'),
+            ),
+          );
+  }
+
+  Widget formAmount() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: TextFormField(
+        keyboardType: TextInputType.number,
+        decoration: InputDecoration(
+          suffix: ShowTitle(
+            title: 'THB.',
+            textStyle: MyConstant().h2RedStyle(),
           ),
+          label: ShowTitle(title: 'Amount : '),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Container buildExpiresCVC() {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 16),
+      child: Row(
+        children: [
+          buildSizeBox(30),
+          Expanded(
+            child: Column(
+              children: [
+                buildTitle('Expires End : '),
+                formExpiresDate(),
+              ],
+            ),
+          ),
+          buildSizeBox(8),
+          Expanded(
+            child: Column(
+              children: [
+                buildTitle('CVC : '),
+                formCVC(),
+              ],
+            ),
+          ),
+          buildSizeBox(30),
         ],
+      ),
+    );
+  }
+
+  Widget formExpiresDate() {
+    return TextFormField(
+      keyboardType: TextInputType.number,
+      decoration: InputDecoration(
+        hintText: 'xx/xxxx',
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+      ),
+    );
+  }
+
+  Widget formCVC() {
+    return TextFormField(
+      keyboardType: TextInputType.number,
+      decoration: InputDecoration(
+        hintText: 'xxx',
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
       ),
     );
   }
