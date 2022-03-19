7d0e05525de5815c9c5ae01141d19fc282c4b389
wintech <wintech_thekop@hotmail.com>
2022-03-19 19:36:38 +0700

    EP109.1 การโค้ดDartเพื่อสร้างState ตามที่ออกแบบไว้

--- lib/state/confirm_add_wallet.dart
+++ lib/state/confirm_add_wallet.dart
@@ -1,6 +1,12 @@
+// ignore_for_file: prefer_const_constructors, prefer_if_null_operators
+
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:intl/intl.dart';
+import 'package:pichiimall/widgets/show_image.dart';
+import 'package:pichiimall/widgets/show_title.dart';
 
 import '../utility/my_constant.dart';
 
@@ -12,6 +18,24 @@ class ConfirmAddWallet extends StatefulWidget {
 }
 
 class _ConfirmAddWalletState extends State<ConfirmAddWallet> {
+  late String dateTimeStr;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    findCurrentTime();
+  }
+
+  void findCurrentTime() {
+    DateTime dateTime = DateTime.now();
+    DateFormat dateFormat = DateFormat('dd/MM/yyyy HH:mm');
+    setState(() {
+      dateTimeStr = dateFormat.format(dateTime);
+    });
+    // print(dateTimeStr);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,6 +49,54 @@ class _ConfirmAddWalletState extends State<ConfirmAddWallet> {
               : Icon(Icons.arrow_back),
         ),
       ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          newHeader(),
+          newDateTime(),
+          Spacer(),
+          newImages(),
+          Spacer(),
+          newButtonConfirm(),
+        ],
+      ),
+    );
+  }
+
+  Container newButtonConfirm() {
+    return Container(width: double.infinity,
+          child: ElevatedButton(
+            onPressed: () {},
+            child: Text('Confirm Add Wallet'),
+          ),
+        );
+  }
+
+  Row newImages() {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        IconButton(onPressed: () {}, icon: Icon(Icons.add_a_photo)),
+        // SvgPicture.asset('images/bill.svg'),
+        Container(
+            width: 200, height: 200, child: ShowImage(path: 'images/bill.png')),
+        IconButton(onPressed: () {}, icon: Icon(Icons.add_photo_alternate)),
+      ],
+    );
+  }
+
+  ShowTitle newDateTime() {
+    return ShowTitle(
+      title: dateTimeStr == null ? 'dd/MM/yy HH:mm' : dateTimeStr,
+      textStyle: MyConstant().h2BlueStyle(),
+    );
+  }
+
+  ShowTitle newHeader() {
+    return ShowTitle(
+      title: 'Current Date Pay',
+      textStyle: MyConstant().h1Style(),
     );
   }
 }
