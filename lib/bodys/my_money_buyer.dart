0ebafd9557a916d7ca96bea1977e5ebe406dd5e0
wintech <wintech_thekop@hotmail.com>
2022-04-02 15:34:20 +0700

    EP121 การดึงข้อมูล Json จาก API มาแสดงใน Flutter

--- lib/bodys/my_money_buyer.dart
+++ lib/bodys/my_money_buyer.dart
@@ -1,10 +1,12 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:pichiimall/bodys/approved.dart';
 import 'package:pichiimall/bodys/waitting.dart';
 import 'package:pichiimall/bodys/wallet.dart';
 import 'package:pichiimall/utility/my_constant.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class MyMoneyBuyer extends StatefulWidget {
   const MyMoneyBuyer({Key? key}) : super(key: key);
@@ -34,7 +36,19 @@ class _MyMoneyBuyerState extends State<MyMoneyBuyer> {
   void initState() {
     // TODO: implement initState
     super.initState();
+    readAllWallet();
     setUpBottonBar();
+  }
+
+  Future<Null> readAllWallet() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    var idBuyer = preferences.getString("id");
+    print(idBuyer);
+    var path =
+        "${MyConstant.domain}/pichiimall/getWalletWhereIdBuyer.php?isAdd=true&idBuyer=$idBuyer";
+    await Dio().get(path).then((value) {
+      print(value);
+    });
   }
 
   void setUpBottonBar() {
